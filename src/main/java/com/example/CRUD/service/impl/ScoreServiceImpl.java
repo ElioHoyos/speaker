9c786b7fb0827b26828c100bda5dc532aabdfde1
portuguez29 <portuguezei@gmail.com>
2024-11-29 18:41:45 -0500

    Update ScoreServiceImpl.java

--- src/main/java/com/example/CRUD/service/impl/ScoreServiceImpl.java
+++ src/main/java/com/example/CRUD/service/impl/ScoreServiceImpl.java
@@ -45,56 +45,36 @@ public class ScoreServiceImpl implements ScoreService {
                     lisErrors.add(new NotFoundException("Student no found with id :: " + requestDao.getIdStudent()));
                     return null;
                 });
-                //.orElseThrow(() -> new StudentNotFoundException(requestDao.getIdStudent()));
+        //.orElseThrow(() -> new StudentNotFoundException(requestDao.getIdStudent()));
         Speaker speaker = speakerRepository.findById(requestDao.getIdSpeaker())
                 .orElseGet(() -> {
                     lisErrors.add(new NotFoundException("Speaker not found with id :: " + requestDao.getIdSpeaker()));
                     return null;
                 });
-                //.orElseThrow(() -> new SpeakerNotFoundException(requestDao.getIdSpeaker()));
+        //.orElseThrow(() -> new SpeakerNotFoundException(requestDao.getIdSpeaker()));
 
-        if(!ObjectUtils.isEmpty(lisErrors)){
+
+        // Intentar evaluar el score y capturar posibles errores
+        ScoreType scoreType = evaluateScore(requestDao.getScore(), lisErrors);
+        if (!ObjectUtils.isEmpty(lisErrors)) {
             throw new NotFoundException(lisErrors);
         }
-
-        try {
-            // Intentar evaluar el score y capturar posibles errores
-            ScoreType scoreType = evaluateScore(requestDao.getScore());
-
-            // Continuar con la creación del Score si no hay errores
-            Score score = Score.builder()
-                    .score(requestDao.getScore())
-                    .date(LocalDate.now())
-                    .scoreType(scoreType)
-                    .student(student)
-                    .speaker(speaker)
-                    .build();
-            scoreRepository.save(score);
-
-        } catch (IllegalArgumentException e) {
-            // Agregar el mensaje de error a la lista de errores
-            lisErrors.add(new NotFoundException(e.getMessage()));
-        }
+        // Continuar con la creación del Score si no hay errores
+        Score score = Score.builder()
+                .score(requestDao.getScore())
+                .date(LocalDate.now())
+                .scoreType(scoreType)
+                .student(student)
+                .speaker(speaker)
+                .build();
+        scoreRepository.save(score);
     }
 
-//    private ScoreType getScoreTypeByScore(Long score){
-//        return switch ((int) (score / 30)) {
-//            case 0 -> // 0 - 30
-//                    ScoreType.BAD;
-//            case 1 -> // 31 - 60
-//                    ScoreType.REGULAR;
-//            case 2 -> // 61 - 90
-//                    ScoreType.GOOD;
-//            case 3 -> // 91 - 100
-//                    ScoreType.GOOD; // Puedes decidir cómo manejar scores > 100
-//            default -> throw new IllegalArgumentException("Score must be between 0 and 100.");
-//        };
-//    }
-
-    public static ScoreType evaluateScore(long score) {
+    public static ScoreType evaluateScore(long score, List<NotFoundException> lisErrors) {
         // Validar si el score está fuera del rango esperado
         if (score < 0 || score > 100) {
-            throw new IllegalArgumentException("La puntuación debe estar entre 0 y 100.");
+            lisErrors.add(new NotFoundException("Score no permitido :: [" + score + "] debería estar entre 0 y 100"));
+            return null;
         }
 
         return switch ((int) (score / 30)) {
@@ -106,25 +86,5 @@ public class ScoreServiceImpl implements ScoreService {
             default -> throw new IllegalStateException("Unexpected value: " + (int) (score / 30));
         };
     }
-
-    public static void main(String[] args) {
-        long score = 200; // Cambia este valor para probar diferentes casos
-        try {
-            ScoreType result = evaluateScore(score);
-            System.out.println("El tipo de puntuación es: " + result);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    public void someMethod(Long speakerId, Long studentId) {
-        if (speakerId == null) {
-            throw new SpeakerNotFoundException("No se encontró el speaker.");
-        }
-        if (studentId == null) {
-            throw new StudentNotFoundException("No se encontró el estudiante.");
-        }
-    }
-
 
 }
